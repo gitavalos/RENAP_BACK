48836fe04fdcbff29fd66289c0ca5696146dc86b
RicH <g.bboyrich@gmail.com>
2018-12-17 01:22:37 +0100

    WEB SERVICE LUGAR
    SE AGREGO SERVICIO DE DEPARTAMENTO Y MUNICIPIO

---
+++ webserviceLugar/src/Controller/BaseController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class BaseController extends AbstractController
+{
+    const NOMBRE_SERVIDOR = "35.193.84.232";
+    const USUARIO_DB = "SAuser";
+	const PASSWORD_DB = "SAuser2018";
+    const NOMBRE_DB = "SA2018";
+
+    public function getConexion(){
+        $mysqli = new \mysqli( 
+            self::NOMBRE_SERVIDOR, 
+            self::USUARIO_DB,
+            self::PASSWORD_DB,
+            self::NOMBRE_DB
+        );
+        return $mysqli;
+    }
+
+    /**
+    * @Route("/sa/tipolugar", name="geTipoLugar")
+    */
+    public function geTipoLugar(Request $request)
+    {
+        $salida = array();
+		$salida['status'] = "-1";
+        $salida['mensaje'] = "fail";
+        $salida['data'] = array();
+        $tipoLugar = array();
+        
+        $mysqli = $this->getConexion();
+        if ($mysqli->connect_errno) {
+            $salida['mensaje'] = "error de conexion";
+        } else {
+            if ($mysqli->multi_query("select * from tipo_lugar" )) {
+                if ($resultado = $mysqli->use_result()) {
+                    while ($fila = $resultado->fetch_row()) {
+                        $tipo = array();
+                        $tipo['codigo'] = $fila[0];
+                        $tipo['nombre'] = $fila[1];
+                        array_push($tipoLugar, $tipo);
+                    }
+                    $resultado->close();
+
+                    $salida['status'] = "1";
+                    $salida['mensaje'] = "OK";
+                    $salida['data'] = $tipoLugar;
+                }
+            } else {
+                $salida['mensaje'] = "error de consulta";
+            }
+            $mysqli->close();
+        }
+        return $this->json($salida);
+    }
+
+}
+
+
+
---
+++ webserviceLugar/src/Controller/LugarController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class LugarController extends BaseController
+{
+
+    /**
+    * @Route("/sa/departamentos", name="geDepartamentos", methods="POST")
+    */
+    public function geDepartamentos(Request $request)
+    {
+        $salida = array();
+		$salida['status'] = "-1";
+        $salida['mensaje'] = "fail";
+        $salida['data'] = array();
+        $departamentos = array();
+        $pais = $request->request->get('pais');
+        if(isset($pais) ){
+            $mysqli = $this->getConexion();
+            if ($mysqli->connect_errno) {
+                $salida['mensaje'] = "error de conexion";
+            } else {
+                $query = "select l.idlugar, l.nombre from ".
+                "lugar l " . 
+                "inner join lugar lp " .
+                "    on l.padre = lp.idlugar " .
+                "where l.idtipo_lugar = 2 " .
+                " and lp.idlugar = " . $pais . 
+                ";";
+                if ($mysqli->multi_query($query )) {
+                    if ($resultado = $mysqli->use_result()) {
+                        while ($fila = $resultado->fetch_row()) {
+                            $tipo = array();
+                            $tipo['codigo'] = $fila[0];
+                            $tipo['nombre'] = $fila[1];
+                            array_push($departamentos, $tipo);
+                        }
+                        $resultado->close();
+
+                        $salida['status'] = "1";
+                        $salida['mensaje'] = "OK";
+                        $salida['data'] = $departamentos;
+                    }
+                } else {
+                    $salida['mensaje'] = "error de consulta";
+                }
+                $mysqli->close();
+            }    
+        }else {
+            $salida['mensaje'] = "no se envio el paramentro";
+        }        
+        return $this->json($salida);
+    }
+    /**
+    * @Route("/sa/municipios", name="geMunicipios", methods="POST")
+    */
+    public function geMunicipios(Request $request)
+    {
+        $salida = array();
+		$salida['status'] = "-1";
+        $salida['mensaje'] = "fail";
+        $salida['data'] = array();
+        $departamentos = array();
+        $departamento = $request->request->get('departamento');
+        if(isset($departamento) ){
+            $mysqli = $this->getConexion();
+            if ($mysqli->connect_errno) {
+                $salida['mensaje'] = "error de conexion";
+            } else {
+                $query = "select l.idlugar, l.nombre from ".
+                "lugar l " . 
+                "inner join lugar lp " .
+                "    on l.padre = lp.idlugar " .
+                "where l.idtipo_lugar = 3 " .
+                " and lp.idlugar = " . $departamento . 
+                ";";
+                if ($mysqli->multi_query($query )) {
+                    if ($resultado = $mysqli->use_result()) {
+                        while ($fila = $resultado->fetch_row()) {
+                            $tipo = array();
+                            $tipo['codigo'] = $fila[0];
+                            $tipo['nombre'] = $fila[1];
+                            array_push($departamentos, $tipo);
+                        }
+                        $resultado->close();
+
+                        $salida['status'] = "1";
+                        $salida['mensaje'] = "OK";
+                        $salida['data'] = $departamentos;
+                    }
+                } else {
+                    $salida['mensaje'] = "error de consulta";
+                }
+                $mysqli->close();
+            }    
+        }else {
+            $salida['mensaje'] = "no se envio el paramentro";
+        }        
+        return $this->json($salida);
+    }
+
+}
+
+
+
