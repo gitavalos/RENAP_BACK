a910663491f591a6703d61c282bbd4ca07ba69ec
RicH <g.bboyrich@gmail.com>
2018-12-17 18:31:53 +0100

    test 3 de integracion matrimonio
    se agrego una tercera prueba de integracion matrimoio

--- webserviceMatrimonio/tests/MatrimonioControllerIntegracionTest.php
+++ webserviceMatrimonio/tests/MatrimonioControllerIntegracionTest.php
@@ -25,4 +25,20 @@ class MatrimonioControllerIntegracionTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
+
+    public function testMatrimonioIntegracion3()
+    {
+        $client = static::createClient();
+
+        $client->request(
+            'POST',
+            '/sa/matrimonio',
+            array('cui' => '1000061831710')
+        );
+
+        $result = json_decode($client->getResponse()->getContent());
+        $stringResult =  '{"status":"1","mensaje":"OK","data":[{"estado":"1","cuiesposo":"1000061831710","cuiesposa":"1000026681303","fechamatrimonio":"1995-12-27 18:05:16","ocupacionesposo":null,"ocupacionesposa":null,"regimenmatrimonial":"comunidad absoluta de bienes","lugardematrimonio":"1224","autoridad":null}]}';
+
+        $this->assertEquals($stringResult, json_encode($result));
+    }
 }
