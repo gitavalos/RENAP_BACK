7a26cc283fbb691a276beef393804b4362454f8e
Alex Gutierrez <ecys.seminario1@gmail.com>
2018-12-17 10:10:25 -0600

    Modificacion de clase Test de Defuncion

--- webserviceDefuncion/tests/RegistroDefuncionControllerTest.php
+++ webserviceDefuncion/tests/RegistroDefuncionControllerTest.php
@@ -10,7 +10,7 @@ class RegistroDefuncionControllerTest extends TestCase
     public function testRegistroDefuncion()
     {
         $registroDefuncion = new RegistroDefuncionController();
-        $result = $registroDefuncion->registroDefuncion("1000000100108", "'1000000100108'", 112, "Mi Casita", 
+        $result = $registroDefuncion->registroDefuncion("1000000100108", "'1000108571605'", 112, "Mi Casita", 
         "2018-01-01 10:15:34", "muerte natural");
 
         $this->assertEquals('-1', $result["status"]);
