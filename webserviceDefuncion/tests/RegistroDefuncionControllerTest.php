d36a67b0ca0277040ea09f0b3f12d5bbbc594908
Alex Gutierrez <ecys.seminario1@gmail.com>
2018-12-17 11:02:19 -0600

    Agregacion de test dentro de RegistroDefuncionControllerTest

--- webserviceDefuncion/tests/RegistroDefuncionControllerTest.php
+++ webserviceDefuncion/tests/RegistroDefuncionControllerTest.php
@@ -15,5 +15,14 @@ class RegistroDefuncionControllerTest extends TestCase
 
         $this->assertEquals('-1', $result["status"]);
     }
-    
+
+    public function testRegistroDefuncion2()
+    {
+        $registroDefuncion = new RegistroDefuncionController();
+        $result = $registroDefuncion->registroDefuncion("1000000291504", "'1000108571605'", 602, "Hospital regional", 
+        "2018-01-01 10:15:34", "Accidente de transito");
+
+        $this->assertEquals('-1', $result["status"]);
+    }
+
 }
